6ee8a29fbf85aee382a6e40ba8b59e85799cff5a
VIoneyy <vioneyy.vio6@gmail.com>
2025-10-30 04:05:18 +0700

    fix(personality): valid JSON persona + clean package import

--- src/personality/__init__.py
+++ src/personality/__init__.py
@@ -1,1 +1,8 @@
-# Personality system
+"""
+Personality package
+- Provides `PersonalityManager` and `Personality` loader for persona.json
+"""
+
+from .personality import PersonalityManager, Personality
+
+__all__ = ["PersonalityManager", "Personality"]
